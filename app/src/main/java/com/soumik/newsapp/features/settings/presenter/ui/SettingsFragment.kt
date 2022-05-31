a89cfa3d5e2d1da5bd48d354165c7a03fdfde30d
soumikBhatt <soumikcse07@gmail.com>
2022-05-31 18:09:36 +0600

    working on setting page ui

--- app/src/main/java/com/soumik/newsapp/features/settings/presenter/ui/SettingsFragment.kt
+++ app/src/main/java/com/soumik/newsapp/features/settings/presenter/ui/SettingsFragment.kt
@@ -1,11 +1,10 @@
 package com.soumik.newsapp.features.settings.presenter.ui
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.soumik.newsapp.R
 import com.soumik.newsapp.features.settings.presenter.viewmodel.SettingsViewModel
 
@@ -24,10 +23,8 @@ class SettingsFragment : Fragment() {
         return inflater.inflate(R.layout.settings_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(SettingsViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
 
 }
