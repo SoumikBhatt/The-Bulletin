4b769e2c7ddfaa06e8369d7e8b1e8b9d26177d0a
soumikBhatt <soumikcse07@gmail.com>
2022-06-01 15:10:04 +0600

    ui adjusted for supporting mode dark and light mode

--- app/src/main/java/com/soumik/newsapp/features/settings/presenter/ui/SettingsFragment.kt
+++ app/src/main/java/com/soumik/newsapp/features/settings/presenter/ui/SettingsFragment.kt
@@ -1,10 +1,12 @@
 package com.soumik.newsapp.features.settings.presenter.ui
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.soumik.newsapp.BuildConfig
 import com.soumik.newsapp.NewsApp
 import com.soumik.newsapp.R
 import com.soumik.newsapp.databinding.SettingsFragmentBinding
@@ -35,9 +37,17 @@ class SettingsFragment : Fragment() {
         (requireActivity().application as NewsApp).appComponent.inject(this)
 
         init()
+        setViews()
         setUpObservers()
         setViewListeners()
 
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun setViews() {
+        mBinding.apply {
+            tvMenuVersionDesc.text = "${BuildConfig.VERSION_NAME} (${BuildConfig.VERSION_CODE})"
+        }
     }
 
     private fun init() {
@@ -50,11 +60,13 @@ class SettingsFragment : Fragment() {
                 if (it) {
                     mBinding.apply {
                         tvMenuTheme.text = getString(R.string.light_mode)
+                        tvMenuThemeDesc.text = getString(R.string.switch_app_theme_to_light_mode)
                         tvMenuTheme.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,R.drawable.ic_light_mode,0)
                     }
                 } else {
                     mBinding.apply {
                         tvMenuTheme.text = getString(R.string.dark_mode)
+                        tvMenuThemeDesc.text = getString(R.string.switch_app_theme_to_dark_mode)
                         tvMenuTheme.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0,R.drawable.ic_dark_mode,0)
                     }
                 }
