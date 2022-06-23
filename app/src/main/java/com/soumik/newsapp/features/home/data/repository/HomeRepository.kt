9b0a39e5f31e0ecf1c714690462bbcbd8858f4b4
Soumik Bhattacharjee <soumikcse07@gmail.com>
2022-06-24 01:03:16 +0600

    optimization

--- app/src/main/java/com/soumik/newsapp/features/home/data/repository/HomeRepository.kt
+++ app/src/main/java/com/soumik/newsapp/features/home/data/repository/HomeRepository.kt
@@ -1,14 +1,8 @@
 package com.soumik.newsapp.features.home.data.repository
 
-import androidx.lifecycle.LiveData
-import androidx.paging.Pager
 import androidx.paging.PagingData
-import com.soumik.newsapp.core.utils.Resource
 import com.soumik.newsapp.features.home.domain.model.Article
-import com.soumik.newsapp.features.home.domain.model.NewsModel
-import io.reactivex.rxjava3.core.Flowable
 import kotlinx.coroutines.flow.Flow
-import retrofit2.Response
 
 /**
 created by Soumik on 3/22/2022
